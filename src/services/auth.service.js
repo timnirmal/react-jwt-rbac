18415875a04fefc7ffea92b4015507266ce13c40
timnirmal <timnirmal@gmail.com>
2022-02-09 18:56:34 +0530

    Authentication service

---
+++ src/services/auth.service.js
@@ -0,0 +1,40 @@
+// Authentication service
+// The service uses Axios for HTTP requests and Local Storage for user information & JWT.
+// It provides following important functions:
+//      login(): POST {username, password} & save JWT to Local Storage
+//      logout(): remove JWT from Local Storage
+//      register(): POST {username, email, password}
+//      getCurrentUser(): get stored user information (including JWT)
+
+import axios from "axios";
+const API_URL = "http://localhost:8081/api/auth/";
+
+const register = (username, email, password) => {
+    return axios.post(API_URL + "signup", {username, email, password});
+};
+
+const login = (username, password) => {
+    return axios
+        .post(API_URL + "signin", {username, password})
+        .then(response => {
+            if (response.data.token) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+            return response.data;
+        });
+};
+
+const logout = () => {
+    localStorage.removeItem("user");
+};
+
+const getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem("user"))
+};
+
+export default {
+    register,
+    login,
+    logout,
+    getCurrentUser
+}
